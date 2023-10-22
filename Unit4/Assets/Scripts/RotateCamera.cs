ea259908e11948a2c78ca6e3f9b2ab3a521959d3
DenysRudenko <heartbreathing19951014@gmail.com>
2023-10-22 22:40:30 +0100

    Created a playerController script..

--- Unit4/Assets/Scripts/RotateCamera.cs
+++ Unit4/Assets/Scripts/RotateCamera.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
-{
+{   
+    public float rotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class RotateCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
